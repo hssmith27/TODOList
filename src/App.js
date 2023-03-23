f6b82889fb3cd63e7ae25272b9c6a98d12e940e7
hssmith27 <hssmith2704@gmail.com>
2023-03-23 16:34:41 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import styled from "styled-components";
+
+function App() { 
+  return (
+    <div className="App">
+      Hello World! Starting a TODOList
+    </div>
+  )
+}
+export default App;
